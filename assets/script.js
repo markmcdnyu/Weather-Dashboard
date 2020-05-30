60e60987e1a4cdb4f9f8b1c230fc91064c7d7ef1
Mark McDonald <markwilliammcdonald@gmail.com>
2020-05-30 04:16:29 -0500

    added the main ajax call for the weather API. Populate the icon. Converted the temp to F and wind speed to MPH. Next is the 5 day loop.

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,61 @@
 // Declare variables here
+//global empty array
+var cityName = [];
 
+//need an emplty string for to store the name of the searched city
+var lastCity = "";
+
+//variable for the API key
+var API = "c2d54620a6b0445bd3d065fa647633a3";
+
+//Trying to see if I need to set the variable for the API url....might not need it as a global variable
+var queryURL = "https://api.openweathermap.org/data/2.5/forecast?appid=" + API + "&q=";
 
 
 //Remeber to call the storage function here
-
-
-
-//Reminder my open weather api KEY is  c2d54620a6b0445bd3d065fa647633a3
-
+storeCity();
 
 
 //function for the ajax call here
     //within this function will be a lot
-    
-    //some conversions to remember 
-    //kelvin to F
-    //wind speed into MPH
+function makeAjaxCall(){
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?appid=" + API + "&q=" + lastCity;
 
-//for loop to populate items but also momentjs() for the time 
+    // This ajax call will actually pull the weather data without the UV data from the API.
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+    }).then(function (response) {
+        $("#city").html(
+            `${lastCity} ${moment().format(
+                "(M/D/YYY)"
+            )} <img src =""https://openweathermap.org/img/wn/${
+				response.list[0].weather[0].icon
+			}@2x.png"/>`
+        );
+        
+        //kelvin to F conversion
+        let tempF = (response.list[0].main.temp -273.15) * 1.8 + 32;
+
+        //now take the temp and push to the dashboard
+        $("#temp").html("Temperature: " + tempF.toFixed[0] + " °F"); //degree symbol on Mac is shift opt 8
+
+        //take the humidity and push to dashboard
+        $("#humidity").html("Humidity: ") + response.list[0].main.humidity + "%");
+
+        //REMEMBER: wind speed into MPH! 
+        let milesPerHR = response.list[0].wind.speed * 2.237;
+
+        //take wind speed and push to dashboard
+        $("wind").html("Wind Speed: " + milesPerHR.toFixed(1) + " MPH");
+        
+
+        //for loop to populate items for the 5 daya forecast and also momentjs() for the time 
+
+    })
+}
+
+
 
 
 //It might be that the UV will be different API call because the forecast call is https://api.openweathermap.org/data/2.5/forecast?appid=
