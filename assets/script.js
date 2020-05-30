719f200190ffbd5ac90eab9ac3811a0f430927a5
Mark McDonald <markwilliammcdonald@gmail.com>
2020-05-30 03:42:28 -0500

    added in the AC to the js file

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,35 @@
-// Fill in with the acceptance Criteria
+// Declare variables here
+
+
+
+//Remeber to call the storage function here
+
 
 
 //Reminder my open weather api KEY is  c2d54620a6b0445bd3d065fa647633a3
+
+
+
+//function for the ajax call here
+    //within this function will be a lot
+    
+    //some conversions to remember 
+    //kelvin to F
+    //wind speed into MPH
+
+//for loop to populate items but also momentjs() for the time 
+
+
+//It might be that the UV will be different API call because the forecast call is https://api.openweathermap.org/data/2.5/forecast?appid=
+//but the uv call might need to be https://api.openweathermap.org/data/2.5/uvi/history?appid= 
+//Yes, seems to be the case for 2 different calls 
+
+
+// need a function to to make the recent searched city list 
+    //within this will most likely need a for loop to create new button for a recent search to go back to and click on
+    //will also need to append or prepend the list
+
+
+//on click event in jquery to search btn
+
+//need function to pull the recently searched into the cycle.  Also need to make Austin the default
