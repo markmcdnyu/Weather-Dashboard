3953f9111e13a78e0645cb924dc7f5349cd0038b
Mark McDonald <markwilliammcdonald@gmail.com>
2020-05-30 22:30:53 -0500

    tweaked the html script. needed a jQuery min that also had Ajax. Will add in the js code I thought was snytax flawed now

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,25 @@
 // Declare variables here
-//global empty array
+//global empty array for storage purposes 
 var cityName = [];
 
 //need an emplty string for to store the name of the searched city
-var lastCity = "";
+var lastCity = "Austin";
 
 //variable for the API key
-var API = "c2d54620a6b0445bd3d065fa647633a3";
+var APIkey = "c2d54620a6b0445bd3d065fa647633a3";
 
 //Trying to see if I need to set the variable for the API url....might not need it as a global variable
 // var queryURL = "https://api.openweathermap.org/data/2.5/forecast?appid=" + API + "&q=";
 
 
 //Remeber to call the storage function here
-storeCity();
+// storeCity();
 
 
 //function for the ajax call here
     //within this function will be a lot
 function makeAjaxCall() {
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?appid=" + API + "&q=" + lastCity;
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?&q=" + lastCity + "&appid=" + APIkey;
 
     // This ajax call will actually pull the weather data without the UV data from the API.
     $.ajax({
@@ -48,10 +48,11 @@ function makeAjaxCall() {
 
         //take wind speed and push to dashboard
         $("wind").html("Wind Speed: " + milesPerHR.toFixed(1) + " MPH");
-        
+        console.log(response);
     });
      // console log for    
     
     //for loop to populate items for the 5 day forecast and also momentjs() for the time
 }
+makeAjaxCall();
 
